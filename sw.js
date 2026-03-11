edac2262e6b6de842384953edb8def56e44e0472
jataidesja-hub <jataides.ja@gmail.com>
2026-03-11 16:16:21 -0300

    fix: melhora busca de endereço, localização inicial e força atualização PWA

--- sw.js
+++ sw.js
@@ -1,14 +1,25 @@
-const CACHE_NAME = 'taximetro-v1';
-const ASSETS = [
+const CACHE_NAME = 'taximetro-v1.2'; // Incrementado para v1.2 para forçar atualização
+const STATIC_ASSETS = [
     './',
     './index.html',
-    './manifest.json'
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
         })
     );
 });
